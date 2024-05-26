3b7c61c2be8dbdcecec734b87a1101b95e6047ad
rzirvi@caltech.edu <rzirvi@caltech.edu>
2024-05-26 21:45:46 +0000

    hard

---
+++ include/constants.h
@@ -0,0 +1,29 @@
+#include "math.h"
+#include "sdl_wrapper.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+extern const vector_t MIN;
+extern const vector_t MAX;
+extern const vector_t CENTER;
+
+extern const size_t INITIAL_ASSET_CAPACITY;
+
+extern const size_t TEXT_SIZE;
+extern const size_t TEXT_HEIGHT_SCALE;
+extern const size_t NUM_TEXT;
+extern const size_t NUM_BACKGROUNDS;
+extern const size_t NUM_BUTTONS;
+extern const size_t NUM_BUTTONS_HOME;
+extern const size_t NUM_BUTTONS_GAME_OVER;
+
+extern const size_t DEFAULT_WIDTH;
+extern const size_t DEFAULT_HEIGHT;
+
+extern const double INTERVAL;
+
+#endif // CONSTANTS_H
---
+++ library/constants.c
@@ -0,0 +1,20 @@
+#include "constants.h"
+
+const vector_t MIN = {0, 0};
+const vector_t MAX = {1000, 500};
+const vector_t CENTER = {500, 250};
+
+const size_t INITIAL_ASSET_CAPACITY = 10;
+
+const size_t TEXT_SIZE = 50;
+const size_t TEXT_HEIGHT_SCALE = 2;
+const size_t NUM_TEXT = 1;
+const size_t NUM_BACKGROUNDS = 1;
+const size_t NUM_BUTTONS = 1;
+const size_t NUM_BUTTONS_HOME = 1;
+const size_t NUM_BUTTONS_GAME_OVER = 2;
+
+const size_t DEFAULT_WIDTH = 0;
+const size_t DEFAULT_HEIGHT = 0;
+
+const double INTERVAL = 1;
