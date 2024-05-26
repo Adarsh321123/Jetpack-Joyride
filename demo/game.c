b281cc6cbaea2ba3ba255a6fcfe27e12e8a8b44e
rzirvi@caltech.edu <rzirvi@caltech.edu>
2024-05-26 06:24:10 +0000

    Autocommit of game for rzirvi

--- demo/game.c
+++ demo/game.c
@@ -45,14 +45,14 @@ typedef struct button_info {
 void play(state_t *state);
 
 button_info_t button_templates[] = {
-    {.image_path = "assets/black_circle.png",
+    {.image_path = "assets/home_button.png",
      .font_path = "assets/New Athletic M54.ttf",
      .image_box = (SDL_Rect){0, 400, 100, 100},
      .text_box = (SDL_Rect){25, 425, 50, 50},
      .text_color = (rgb_color_t){255, 255, 255},
      .text = "Play",
      .handler = (void *)play},
-    {.image_path = "assets/black_circle.png",
+    {.image_path = "assets/home_button.png",
      .font_path = "assets/New Athletic M54.ttf",
      .image_box = (SDL_Rect){900, 400, 100, 100},
      .text_box = (SDL_Rect){925, 425, 50, 50},
