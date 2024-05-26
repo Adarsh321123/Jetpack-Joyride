9329ea4b8532c0d9ff0e0d5d5c8178b03b18d173
rzirvi@caltech.edu <rzirvi@caltech.edu>
2024-05-26 18:15:05 +0000

    Autocommit of game for rzirvi

--- demo/home.c
+++ demo/home.c
@@ -8,21 +8,21 @@
 #include "asset_cache.h"
 #include "sdl_wrapper.h"
 
-const vector_t MIN = {0, 0};
-const vector_t MAX = {1000, 500};
-const vector_t CENTER = {500, 250};
+// const vector_t MIN = {0, 0};
+// const vector_t MAX = {1000, 500};
+// const vector_t CENTER = {500, 250};
 
-const size_t INITIAL_ASSET_CAPACITY = 10;
+// const size_t INITIAL_ASSET_CAPACITY = 10;
 
-const size_t TEXT_SIZE = 50;
-const size_t TEXT_HEIGHT_SCALE = 2;
-const size_t NUM_BACKGROUNDS = 1;
-const size_t NUM_BUTTONS = 1;
+// const size_t TEXT_SIZE = 50;
+// const size_t TEXT_HEIGHT_SCALE = 2;
+// const size_t NUM_BACKGROUNDS = 1;
+// const size_t NUM_BUTTONS = 1;
 
-const size_t DEFAULT_WIDTH = 0;
-const size_t DEFAULT_HEIGHT = 0;
+// const size_t DEFAULT_WIDTH = 0;
+// const size_t DEFAULT_HEIGHT = 0;
 
-const double INTERVAL = 1;
+// const double INTERVAL = 1;
 
 typedef struct background_info {
   const char *bg_path;
