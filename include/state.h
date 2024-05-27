875bc5c8298dba526164ecc75fb27487e2fc1fe6
dsheth@caltech.edu <dsheth@caltech.edu>
2024-05-27 07:32:56 +0000

    Autocommit of game for dsheth

--- include/state.h
+++ include/state.h
@@ -4,12 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-<<<<<<< HEAD
-typedef struct state state_t;
-=======
 #ifndef STATE_H
 #define STATE_H
->>>>>>> dhruv_clean
 
 /**
  * Stores the demo state
@@ -31,11 +27,7 @@ typedef enum {
  * Returns the pointer to this state (This is the state emscripten_main
  * and emscripten_free work with)
  */
-<<<<<<< HEAD
-game_play_state_t *emscripten_init();
-=======
 state_t *emscripten_init();
->>>>>>> dhruv_clean
 
 /**
  * Called on each tick of the program
@@ -45,11 +37,7 @@ state_t *emscripten_init();
  * @param state pointer to a state object with info about demo
  * @return a boolean representing whether the game/demo is over
  */
-<<<<<<< HEAD
-bool emscripten_main(game_play_state_t *game_play_state);
-=======
 bool emscripten_main(state_t *state);
->>>>>>> dhruv_clean
 
 /**
  * Frees anything allocated in the demo
@@ -57,11 +45,7 @@ bool emscripten_main(state_t *state);
  *
  * @param state pointer to a state object with info about demo
  */
-<<<<<<< HEAD
-void emscripten_free(game_play_state_t *game_play_state);
-=======
 void emscripten_free(state_t *state);
 
 
 #endif // STATE_H
->>>>>>> dhruv_clean
