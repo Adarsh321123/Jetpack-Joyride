3fa3ee161ab1e0fbb318fcba608ff009dc62d8f4
akumarap@caltech.edu <akumarap@caltech.edu>
2024-05-27 23:10:35 +0000

    Autocommit of game for akumarap

--- demo/game_play.c
+++ demo/game_play.c
@@ -150,6 +150,9 @@ bool game_play_main(game_play_state_t *game_play_state) {
   double dt = time_since_last_tick();
   state_temp_t *state = game_play_state->state;
   sdl_clear();
+  for (size_t i = 0; i < list_size(state->body_assets); i++) {
+    asset_render(list_get(state->body_assets, i));
+  }
 
   background_update(state->background_state, dt);
   asset_render(state->background_state->bg1);
