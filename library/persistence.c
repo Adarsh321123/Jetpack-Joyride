7460045e6f26afe75a8bb33ebd3f8f31b75d987a
akumarap@caltech.edu <akumarap@caltech.edu>
2024-06-07 02:06:33 +0000

    able to read and write using achievements struct!

--- library/persistence.c
+++ library/persistence.c
@@ -5,6 +5,8 @@
 #include <emscripten.h>
 #include <string.h>
 
+#include "list.h"
+
 // TODO: add header for this to replicate achievmeents later
 
 typedef struct achievement {
@@ -14,9 +16,9 @@ typedef struct achievement {
     bool unlocked;
 } achievement_t;
 
-// TODO: add observer and list
+// TODO: add observer
 typedef struct achievements {
-    achievement_t *achievement;
+    list_t *achievements_list;
 } achievements_t;
 
 const size_t INITIAL_ACHIEVEMENTS = 5;
@@ -25,6 +27,10 @@ const char *ACHIEVEMENTS_FILENAME = "/persistent/achievements.txt";
 const char *FIRST_ACHIEVEMENT = "Collect 50 Coins|0|50|false";
 const char *SECOND_ACHIEVEMENT = "Travel 1000 meters|0|1000|false";
 const char *THIRD_ACHIEVEMENT = "Dodge 10 Zappers|0|10|false";
+
+size_t achievements_size(achievements_t *achievements) {
+    return list_size(achievements->achievements_list);
+}
 
 void init_achievements_file(achievements_t *achievements) {
   FILE *achievements_file = fopen(ACHIEVEMENTS_FILENAME, "w");
@@ -51,31 +57,33 @@ void read_achievements(achievements_t *achievements) {
     char *line = malloc(sizeof(char) * (char_read + 1));
     while(fgets(line, char_read, achievements_file)) {
         fprintf(stderr, "%s", line);
+        achievement_t *achievement = malloc(sizeof(achievement_t));
         line[strcspn(line, "\n")] = '\0';
         char *token = strtok(line, "|");
         if (token != NULL) {
             // TODO: need strdup?
-            achievements->achievement->name = strdup(token);
-            assert(achievements->achievement->name);
+            achievement->name = strdup(token);
+            assert(achievement->name);
         }
         token = strtok(NULL, "|");
         if (token != NULL) {
-            achievements->achievement->progress = atoi(token);
+            achievement->progress = atoi(token);
         }
         token = strtok(NULL, "|");
         if (token != NULL) {
-            achievements->achievement->target = atoi(token);
+            achievement->target = atoi(token);
         }
         token = strtok(NULL, "|");
         if (token != NULL) {
             bool unlocked = strcmp(token, "true") == 0;
-            achievements->achievement->unlocked = unlocked;
+            achievement->unlocked = unlocked;
         }
+        list_add(achievements->achievements_list, achievement);
     }
     fprintf(stderr, "Read all lines\n");
-    fprintf(stderr, "%s\n", achievements->achievement->name);
-    fprintf(stderr, "%zu\n", achievements->achievement->progress);
-    fprintf(stderr, "%zu\n", achievements->achievement->target);
+    fprintf(stderr, "%s\n", ((achievement_t *)list_get(achievements->achievements_list, 0))->name);
+    fprintf(stderr, "%zu\n", ((achievement_t *)list_get(achievements->achievements_list, 0))->progress);
+    fprintf(stderr, "%zu\n", ((achievement_t *)list_get(achievements->achievements_list, 0))->target);
     int close_result = fclose(achievements_file);  // using int from Adam's example
     assert(close_result == 0);
     free(line);
@@ -85,9 +93,22 @@ void write_achievements(achievements_t *achievements) {
     FILE *achievements_file = fopen(ACHIEVEMENTS_FILENAME, "w");
     assert(achievements_file != NULL);
     fprintf(stderr, "File opened for writing\n");
-    fprintf(achievements_file, "Dodge 10 Zappers|1|10|false\n");
-    fflush(achievements_file);
-    fprintf(stderr, "File written to\n");
+    size_t num_achievements = achievements_size(achievements);
+    for (size_t i = 0; i < num_achievements; i++) {
+        achievement_t *achievement = list_get(achievements->achievements_list, i);
+        char *unlocked = NULL;
+        if (achievement->unlocked) {
+            unlocked = "true";
+        } else {
+            unlocked = "false";
+        }
+        fprintf(achievements_file, "%s|%zu|%zu|%s\n",
+                achievement->name,
+                achievement->progress + 1,
+                achievement->target + 1,
+                unlocked);
+    }
+    fprintf(stderr, "Wrote all lines\n");
 
     int close_result = fclose(achievements_file);
     assert(close_result == 0);
@@ -156,6 +177,8 @@ int main() {
 
     achievements_t *achievements = malloc(sizeof(achievements_t));
     assert(achievements != NULL);
+    achievements->achievements_list = list_init(INITIAL_ACHIEVEMENTS, (free_func_t)list_free);
+    fprintf(stderr, "Initialized achievements list\n");
 
     // mount the persistent filesystem
     mount_persistent_fs();
