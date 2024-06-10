37978445f55a9a48661cf89a4513a979e0046d91
rzirvi@caltech.edu <rzirvi@caltech.edu>
2024-06-10 07:17:55 +0000

    good

--- demo/game_play.c
+++ demo/game_play.c
@@ -622,6 +622,52 @@ static void add_powerup(game_play_state_t *game_play_state, double dt)
 }
 
 /**
+ * Make the things for rocket warning.
+*/
+static void make_rocket_warning(game_play_state_t *game_play_state) {
+  game_play_state->rocket->time_rocket = ZERO;
+  game_play_state->rocket->time_rocket_spawn = game_play_state->time;
+  game_play_state->rocket->rocket_active = false;
+  double y_pos = fmod(rand(), (MAX.y - MIN.y) - ROCKETS_Y_OFFSET) + 
+                                                        ROCKETS_Y_POS_BUFFER;
+  double x_pos = MAX.x - ROCKETS_X_POS_BUFFER;
+  vector_t center = {.x = x_pos, .y = y_pos};
+  game_play_state->rocket->rocket_spawn_position = center;
+  body_t *warning = make_obstacle_rectangle(&center, WARNING_WIDTH, 
+                                  WARNING_HEIGHT, ROCKET_WARNING, VEC_ZERO);
+  scene_add_body(game_play_state->state->scene, warning);
+  asset_update_bounding_box(game_play_state->rocket->rocket_inactive_asset, 
+                            warning);
+  game_play_state->rocket->rocket_inactive = true;
+}
+
+/**
+ * Make the things for rocket moving.
+*/
+static void make_moving_rocket(game_play_state_t *game_play_state) {
+  game_play_state->rocket->time_rocket_activate = game_play_state->time;
+  game_play_state->rocket->time_rocket_spawn = INFINITY;
+  remove_warnings(game_play_state);
+  game_play_state->rocket->rocket_spawn_position.x += ROCKETS_X_POS_BUFFER;
+  body_t *rocket = make_obstacle_rectangle(
+    &game_play_state->rocket->rocket_spawn_position, ROCKET_WIDTH, 
+    ROCKET_HEIGHT, ROCKET, ROCKET_VEL);
+  scene_add_body(game_play_state->state->scene, rocket);
+  asset_t *img = asset_make_image_with_body(ROCKET_PATH, rocket);
+
+  list_add(game_play_state->state->body_assets, img);
+  if (!game_play_state->powerup->powerup_active ||
+      game_play_state->powerup->powerup_type != SHIELD)
+  {
+    create_collision(game_play_state->state->scene, rocket, 
+                    game_play_state->state->user, game_over,
+                      game_play_state, NULL, DEFAULT_FORCE_CONSTANT);
+  }
+  game_play_state->rocket->rocket_inactive = false;
+  game_play_state->rocket->rocket_active = true;
+}
+
+/**
  * Add the rocket to the game.
 */
 static void add_rocket(game_play_state_t *game_play_state, double dt)
@@ -632,45 +678,88 @@ static void add_rocket(game_play_state_t *game_play_state, double dt)
       game_play_state->rocket->time_rocket >= 
                               game_play_state->rocket->rocket_generation_time)
   {
-    game_play_state->rocket->time_rocket = ZERO;
-    game_play_state->rocket->time_rocket_spawn = game_play_state->time;
-    game_play_state->rocket->rocket_active = false;
-    double y_pos = fmod(rand(), (MAX.y - MIN.y) - ROCKETS_Y_OFFSET) + 
-                                                          ROCKETS_Y_POS_BUFFER;
-    double x_pos = MAX.x - ROCKETS_X_POS_BUFFER;
-    vector_t center = {.x = x_pos, .y = y_pos};
-    game_play_state->rocket->rocket_spawn_position = center;
-    body_t *warning = make_obstacle_rectangle(&center, WARNING_WIDTH, 
-                                    WARNING_HEIGHT, ROCKET_WARNING, VEC_ZERO);
-    scene_add_body(game_play_state->state->scene, warning);
-    asset_update_bounding_box(game_play_state->rocket->rocket_inactive_asset, 
-                              warning);
-    game_play_state->rocket->rocket_inactive = true;
+    make_rocket_warning(game_play_state);
   }
 
   else if (game_play_state->rocket->rocket_inactive && game_play_state->time - 
           game_play_state->rocket->time_rocket_spawn >= ROCKETS_ACTIVATION_TIME)
   {
-    game_play_state->rocket->time_rocket_activate = game_play_state->time;
-    game_play_state->rocket->time_rocket_spawn = INFINITY;
-    remove_warnings(game_play_state);
-    game_play_state->rocket->rocket_spawn_position.x += ROCKETS_X_POS_BUFFER;
-    body_t *rocket = make_obstacle_rectangle(
-      &game_play_state->rocket->rocket_spawn_position, ROCKET_WIDTH, 
-      ROCKET_HEIGHT, ROCKET, ROCKET_VEL);
-    scene_add_body(game_play_state->state->scene, rocket);
-    asset_t *img = asset_make_image_with_body(ROCKET_PATH, rocket);
+    make_moving_rocket(game_play_state);
+  }
+}
 
-    list_add(game_play_state->state->body_assets, img);
-    if (!game_play_state->powerup->powerup_active ||
-        game_play_state->powerup->powerup_type != SHIELD)
+/**
+ * Logic to add an inactive laser
+*/
+static void add_laser_inactive(game_play_state_t *game_play_state) {
+  if (game_play_state->laser->time_laser >=
+      game_play_state->laser->laser_generation_time)
+  {
+    game_play_state->laser->laser_active = true;
+    game_play_state->laser->time_laser = ZERO;
+    size_t size_of_spawn_list = list_size(
+                            game_play_state->laser->laser_spawn_positions);
+    for (size_t i = 0; i < size_of_spawn_list; i++)
     {
-      create_collision(game_play_state->state->scene, rocket, 
-                      game_play_state->state->user, game_over,
-                       game_play_state, NULL, DEFAULT_FORCE_CONSTANT);
+      list_remove(game_play_state->laser->laser_spawn_positions, 
+                    LIST_START_INDEX);
     }
-    game_play_state->rocket->rocket_inactive = false;
-    game_play_state->rocket->rocket_active = true;
+    game_play_state->laser->time_laser_spawn = game_play_state->time;
+    size_t num_laser_initialize = (size_t)round(fmod(
+                                  rand(), MAX_NUM_LASERS)) + MIN_NUM_LASERS;
+    size_t start_laser_position = (size_t)round(fmod(
+                                rand(), NUM_LASERS - num_laser_initialize));
+
+    for (size_t i = start_laser_position; i < start_laser_position +
+                                                num_laser_initialize; i++)
+    {
+      vector_t *center = list_get(game_play_state->laser->laser_centers, i);
+      list_add(game_play_state->laser->laser_spawn_positions, center);
+      body_t *laser = make_obstacle_rectangle(center, LASER_WIDTH_ACTIVE,
+                                      LASER_HEIGHT_ACTIVE, LASER, VEC_ZERO);
+      scene_add_body(game_play_state->state->scene, laser);
+      asset_update_bounding_box(list_get(
+                  game_play_state->laser->laser_inactive_assets, i), laser);
+    }
+    game_play_state->laser->laser_spawn_bool = LASER_ACTIVE_STATE;
+  }
+}
+
+/**
+ * Logic to add an active laser
+*/
+static void add_active_laser(game_play_state_t *game_play_state) {
+  if (game_play_state->time - game_play_state->laser->time_laser_spawn >=
+      LASER_ACTIVATION_TIME)
+  {
+    game_play_state->laser->time_laser_activate = game_play_state->time;
+    game_play_state->laser->time_laser_spawn = INFINITY;
+
+    remove_lasers_inactive(game_play_state);
+    size_t size_of_spawn_list = list_size(
+                        game_play_state->laser->laser_spawn_positions);
+
+    for (size_t i = 0; i < size_of_spawn_list; i++)
+    {
+      body_t *laser = make_obstacle_rectangle(list_get(
+        game_play_state->laser->laser_spawn_positions, i),
+        LASER_WIDTH_ACTIVE, LASER_HEIGHT_ACTIVE, LASER_ACTIVE, VEC_ZERO);
+      scene_add_body(game_play_state->state->scene, laser);
+      asset_update_bounding_box(list_get(
+        game_play_state->laser->laser_active_assets, i), laser);
+      if (!game_play_state->powerup->powerup_active ||
+          game_play_state->powerup->powerup_type != SHIELD)
+      {
+        create_collision(game_play_state->state->scene, laser, 
+                        game_play_state->state->user, game_over,
+                        game_play_state, NULL, DEFAULT_FORCE_CONSTANT);
+      }
+    }
+    for (size_t i = 0; i < size_of_spawn_list; i++)
+    {
+      list_remove(game_play_state->laser->laser_spawn_positions,
+                  LIST_START_INDEX);
+    }
   }
 }
 
@@ -684,74 +773,13 @@ static void add_laser(game_play_state_t *game_play_state, double dt)
   {
     case LASER_INACTIVE_STATE:
     {
-      if (game_play_state->laser->time_laser >=
-          game_play_state->laser->laser_generation_time)
-      {
-        game_play_state->laser->laser_active = true;
-        game_play_state->laser->time_laser = ZERO;
-        size_t size_of_spawn_list = list_size(
-                                game_play_state->laser->laser_spawn_positions);
-        for (size_t i = 0; i < size_of_spawn_list; i++)
-        {
-          list_remove(game_play_state->laser->laser_spawn_positions, 
-                        LIST_START_INDEX);
-        }
-        game_play_state->laser->time_laser_spawn = game_play_state->time;
-        size_t num_laser_initialize = (size_t)round(fmod(
-                                      rand(), MAX_NUM_LASERS)) + MIN_NUM_LASERS;
-        size_t start_laser_position = (size_t)round(fmod(
-                                    rand(), NUM_LASERS - num_laser_initialize));
-
-        for (size_t i = start_laser_position; i < start_laser_position +
-                                                    num_laser_initialize; i++)
-        {
-          vector_t *center = list_get(game_play_state->laser->laser_centers, i);
-          list_add(game_play_state->laser->laser_spawn_positions, center);
-          body_t *laser = make_obstacle_rectangle(center, LASER_WIDTH_ACTIVE,
-                                          LASER_HEIGHT_ACTIVE, LASER, VEC_ZERO);
-          scene_add_body(game_play_state->state->scene, laser);
-          asset_update_bounding_box(list_get(
-                      game_play_state->laser->laser_inactive_assets, i), laser);
-        }
-        game_play_state->laser->laser_spawn_bool = LASER_ACTIVE_STATE;
-      }
+      add_laser_inactive(game_play_state);
       break;
     }
 
     case LASER_ACTIVE_STATE:
     {
-      if (game_play_state->time - game_play_state->laser->time_laser_spawn >=
-          LASER_ACTIVATION_TIME)
-      {
-        game_play_state->laser->time_laser_activate = game_play_state->time;
-        game_play_state->laser->time_laser_spawn = INFINITY;
-
-        remove_lasers_inactive(game_play_state);
-        size_t size_of_spawn_list = list_size(
-                            game_play_state->laser->laser_spawn_positions);
-
-        for (size_t i = 0; i < size_of_spawn_list; i++)
-        {
-          body_t *laser = make_obstacle_rectangle(list_get(
-            game_play_state->laser->laser_spawn_positions, i),
-            LASER_WIDTH_ACTIVE, LASER_HEIGHT_ACTIVE, LASER_ACTIVE, VEC_ZERO);
-          scene_add_body(game_play_state->state->scene, laser);
-          asset_update_bounding_box(list_get(
-            game_play_state->laser->laser_active_assets, i), laser);
-          if (!game_play_state->powerup->powerup_active ||
-              game_play_state->powerup->powerup_type != SHIELD)
-          {
-            create_collision(game_play_state->state->scene, laser, 
-                            game_play_state->state->user, game_over,
-                            game_play_state, NULL, DEFAULT_FORCE_CONSTANT);
-          }
-        }
-        for (size_t i = 0; i < size_of_spawn_list; i++)
-        {
-          list_remove(game_play_state->laser->laser_spawn_positions,
-                      LIST_START_INDEX);
-        }
-      }
+      add_active_laser(game_play_state);
       break;
     }
     default:
